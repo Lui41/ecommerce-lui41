c7d5ca3c83f1485d2f9a3ae4b2d1a4210a1a9da4
Luis1303 <luialvarez42@gmail.com>
2026-07-16 16:42:47 -0500

    feat:Jwt agragado en usermodule

--- ecommerce_Lui41/src/users/users.module.ts
+++ ecommerce_Lui41/src/users/users.module.ts
@@ -4,9 +4,10 @@ import { UsersController } from './users.controller';
 import { UsersRepository } from './users.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [JwtModule.register({}),TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UsersService, UsersRepository],
   exports: [UsersRepository],
